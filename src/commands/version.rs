9e3d5b8c92fd7e4da747c246652b0fcdb3a6af94
nulluser <diuyeung@gmail.com>
2024-05-23 13:50:36 +1000

    change to modman

--- src/commands/version.rs
+++ src/commands/version.rs
@@ -3,7 +3,7 @@ use colored::Colorize;
 use crate::errors::ModManError;
 
 pub fn command_version() -> Result<(), ModManError> {
-    println!("{} ({})", "WinForge".yellow(), env!("CARGO_PKG_NAME").bright_black());
+    println!("{} ({})", "ModMan".yellow(), env!("CARGO_PKG_NAME").bright_black());
     println!("v{}", env!("CARGO_PKG_VERSION"));
     println!("{}", env!("CARGO_PKG_DESCRIPTION"));
     println!("Author: {}", env!("CARGO_PKG_AUTHORS"));
